44e26f5f777976f887aa6d1d153c1fbee5d800ca
Daslin Davidson <daslin@appexert.com>
2023-07-12 19:42:53 +0530

    Update TalentPool page

--- components/talent-pool/hire-service.tsx
+++ components/talent-pool/hire-service.tsx
@@ -1,195 +1,59 @@
-import Container from "@components/container";
+import Image from "next/image";
 
 const HireService = () => {
   return (
-    <section className='py-8 md:py-16 lg:px-10 text-center  bg-shaped '>
-      <Container >
-        <div className=' pb-6'>
-          <div className='section-heading inline relative text-white'>
-            Revolutionizing Tech Hiring
+    <section className='md:pt-[70px] mt-[36px] lg:px-10 text-center '>
+      <div className='flex flex-col place-items-center text-center'>
+        <p className='mt-2 sub-heading  '>
+          Revolutionizing Tech Hiring
+        </p>
+        <p className='mt-2 landing-para text-[18px] '>
+          Hire Great Developers with Zero Upfront Commitment and Flexible Pricing!
+        </p>
+      </div>
+
+      <div className='flex flex-col md:flex-row gap-4 text-gray-400 pt-[48px] md:pt-[80px] justify-center items-center'>
+        <div className="flex items-start gap-2  ">
+          <div className="flex w-[18px] h-[18px] flex-shrink-0">
+            <Image
+              width={18}
+              height={18}
+              src={"/images/home/tic-icon.svg"}
+              alt="Check icon"
+            />
           </div>
-          <p className='section-preheading pt-2 text-white'>
-            Hire Great Developers with Zero Upfront Commitment and Flexible
-            Pricing!
+          <p className="-mt-1">
+          Access to a collection of diversified developers
+          </p>
+        </div>
+        <div className="flex items-start gap-2 ">
+          <div className="flex w-[18px] h-[18px] flex-shrink-0">
+            <Image
+              width={18}
+              height={18}
+              src={"/images/home/tic-icon.svg"}
+              alt="Check icon"
+            />
+          </div>
+          <p className="-mt-1">
+          Convenient & swift hiring process
+          </p>
+        </div>
+        <div className="flex items-start gap-2 ">
+          <div className="flex w-[18px] h-[18px] flex-shrink-0">
+            <Image
+              width={18}
+              height={18}
+              src={"/images/home/tic-icon.svg"}
+              alt="Check icon"
+            />
+          </div>
+          <p className="-mt-1">
+          Highly talented developers at a cost-effective budget
           </p>
         </div>
 
-        <div className='flex flex-col items-center py-2  '>
-          <div className='flex flex-col  gap-4 '>
-            <div className=' py-2'>
-              <p className='section-preheading font-semibold text-blue-light  flex '>
-                <svg
-                  className='mr-2'
-                  data-name='Group 1767'
-                  xmlns='http://www.w3.org/2000/svg'
-                  xmlnsXlink='http://www.w3.org/1999/xlink'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 24 24'
-                >
-                  <defs>
-                    <clipPath>
-                      <rect
-                        data-name='Rectangle 659'
-                        width='24'
-                        height='24'
-                        fill='#034f57'
-                        stroke='#55dfc6'
-                        strokeWidth='1'
-                      ></rect>
-                    </clipPath>
-                  </defs>
-                  <g data-name='Group 1766' clipPath='url(#clipPath)'>
-                    <circle
-                      data-name='Ellipse 316'
-                      cx='10.5'
-                      cy='10.5'
-                      r='10.5'
-                      transform='translate(1.5 1.5)'
-                      fill='#55dfc6'
-                      stroke='#55dfc6'
-                      strokeMiterlimit='10'
-                      strokeWidth='2'
-                      opacity='0.25'
-                    ></circle>
-                    <circle
-                      data-name='Ellipse 315'
-                      cx='10.5'
-                      cy='10.5'
-                      r='10.5'
-                      transform='translate(1.5 1.5)'
-                      fill='none'
-                      stroke='#55dfc6'
-                      strokeMiterlimit='10'
-                      strokeWidth='2'
-                    ></circle>
-                    <path
-                      d='M8.614,1.482l-4.7,4.8a.958.958,0,0,1-.595.252H2.91a.96.96,0,0,1-.594-.252L.1,4.018a.362.362,0,0,1,0-.5l.872-.888a.345.345,0,0,1,.493,0L2.882,4.065a.345.345,0,0,0,.493,0L7.252.1a.346.346,0,0,1,.493,0L8.613.98a.361.361,0,0,1,0,.5Z'
-                      transform='translate(8 8.732)'
-                      fill='#55dfc6'
-                    ></path>
-                  </g>
-                </svg>
-                {"Access to a collection of diversified developers"}
-              </p>
-            </div>
-            <div className=' pb-2'>
-              <p className='section-preheading font-semibold text-blue-light  flex '>
-                <svg
-                  className='mr-2'
-                  data-name='Group 1767'
-                  xmlns='http://www.w3.org/2000/svg'
-                  xmlnsXlink='http://www.w3.org/1999/xlink'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 24 24'
-                >
-                  <defs>
-                    <clipPath>
-                      <rect
-                        data-name='Rectangle 659'
-                        width='24'
-                        height='24'
-                        fill='#034f57'
-                        stroke='#55dfc6'
-                        strokeWidth='1'
-                      ></rect>
-                    </clipPath>
-                  </defs>
-                  <g data-name='Group 1766' clipPath='url(#clipPath)'>
-                    <circle
-                      data-name='Ellipse 316'
-                      cx='10.5'
-                      cy='10.5'
-                      r='10.5'
-                      transform='translate(1.5 1.5)'
-                      fill='#55dfc6'
-                      stroke='#55dfc6'
-                      strokeMiterlimit='10'
-                      strokeWidth='2'
-                      opacity='0.25'
-                    ></circle>
-                    <circle
-                      data-name='Ellipse 315'
-                      cx='10.5'
-                      cy='10.5'
-                      r='10.5'
-                      transform='translate(1.5 1.5)'
-                      fill='none'
-                      stroke='#55dfc6'
-                      strokeMiterlimit='10'
-                      strokeWidth='2'
-                    ></circle>
-                    <path
-                      d='M8.614,1.482l-4.7,4.8a.958.958,0,0,1-.595.252H2.91a.96.96,0,0,1-.594-.252L.1,4.018a.362.362,0,0,1,0-.5l.872-.888a.345.345,0,0,1,.493,0L2.882,4.065a.345.345,0,0,0,.493,0L7.252.1a.346.346,0,0,1,.493,0L8.613.98a.361.361,0,0,1,0,.5Z'
-                      transform='translate(8 8.732)'
-                      fill='#55dfc6'
-                    ></path>
-                  </g>
-                </svg>
-                {"Convenient & swift hiring process"}
-              </p>
-            </div>
-            <div className='pb-2'>
-              <p className='section-preheading font-semibold text-blue-light  flex '>
-                <svg
-                  className='mr-2'
-                  data-name='Group 1767'
-                  xmlns='http://www.w3.org/2000/svg'
-                  xmlnsXlink='http://www.w3.org/1999/xlink'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 24 24'
-                >
-                  <defs>
-                    <clipPath>
-                      <rect
-                        data-name='Rectangle 659'
-                        width='24'
-                        height='24'
-                        fill='#034f57'
-                        stroke='#55dfc6'
-                        strokeWidth='1'
-                      ></rect>
-                    </clipPath>
-                  </defs>
-                  <g data-name='Group 1766' clipPath='url(#clipPath)'>
-                    <circle
-                      data-name='Ellipse 316'
-                      cx='10.5'
-                      cy='10.5'
-                      r='10.5'
-                      transform='translate(1.5 1.5)'
-                      fill='#55dfc6'
-                      stroke='#55dfc6'
-                      strokeMiterlimit='10'
-                      strokeWidth='2'
-                      opacity='0.25'
-                    ></circle>
-                    <circle
-                      data-name='Ellipse 315'
-                      cx='10.5'
-                      cy='10.5'
-                      r='10.5'
-                      transform='translate(1.5 1.5)'
-                      fill='none'
-                      stroke='#55dfc6'
-                      strokeMiterlimit='10'
-                      strokeWidth='2'
-                    ></circle>
-                    <path
-                      d='M8.614,1.482l-4.7,4.8a.958.958,0,0,1-.595.252H2.91a.96.96,0,0,1-.594-.252L.1,4.018a.362.362,0,0,1,0-.5l.872-.888a.345.345,0,0,1,.493,0L2.882,4.065a.345.345,0,0,0,.493,0L7.252.1a.346.346,0,0,1,.493,0L8.613.98a.361.361,0,0,1,0,.5Z'
-                      transform='translate(8 8.732)'
-                      fill='#55dfc6'
-                    ></path>
-                  </g>
-                </svg>
-                {"Highly talented developers at a cost-effective budget"}
-              </p>
-            </div>
-          </div>
-        </div>
-      </Container>
+      </div>
     </section>
   );
 };
--- components/talent-pool/talent-pool-hero.tsx
+++ components/talent-pool/talent-pool-hero.tsx
@@ -3,9 +3,11 @@ import GetStartedBanner from "@components/talent-pool/get-started-banner";
 import Container from "@components/container";
 import { Developer } from "@models/developer.model";
 import { getLandingDevelopers } from "@services/developer-api.service";
-import { Input, Skeleton, Row } from "antd";
+import { Input, Skeleton, Row, Button } from "antd";
 import { useEffect, useState } from "react";
 import TalentPoolList from "./talent-pool-list";
+import HireService from "./hire-service";
+import Link from "next/link";
 
 const { Search } = Input;
 const TalentPoolHero = () => {
@@ -47,107 +49,195 @@ const TalentPoolHero = () => {
   }, []);
 
   return (
-    <>
-      <section className='pt-28 '>
-        <Container>
-          <div className='text-center '>
-            <div className='hero-heading '>
-              Hire
-              <span className='text-brand-500 px-2 '>Developers</span>
-              you want{" "}
-            </div>
-            <p className='section-preheading pt-2'>
-              Get instant insights with our in-depth developer profiles & build
-              your team from untapped & quality talents.
+    <Container className="md:py-[100px]  overflow-hidden">
+
+      <section >
+        <div className="text-center flex flex-col justify-center items-center ">
+          <h1 className="heading pt-2 text-center md:max-w-[775px] text-[48px]">
+            Hire qualified developers in record time
+          </h1>
+          <p className='section-paragraph text-[#908E9F]  md:max-w-[672px]  py-[10px] pb-[20px]  '>
+            Get instant insights with our in-depth developer profiles & build your team from untapped & quality talents.
+          </p>
+        </div>
+         
+        <div className='flex justify-center py-10 text-center lg:text-left'>
+          <div className='flex flex-col items-center '>
+            <Search
+              className='talentSearch  mr-2 pb-2 md:pb-0 text-[#605E6E]'
+              allowClear
+              value={searchInputValue}
+              placeholder='Search by skills & roles'                
+              onChange={(value) => {
+                setSearchInputValue(value.target.value);
+              }}
+              onSearch={(value) => {
+                fetchDevelopers(value?.toLowerCase());
+              }}
+            />
+
+            <p className='pt-4 text-[#908E9F] text-[14px]'>
+                Popular categories:
+              <span
+                className='px-[12px] py-1 m-2 font-semibold cursor-pointer rounded-[4px] border-[1px] border-[#252234] hover:border-[#716999]'
+                onClick={() => {
+                  fetchDevelopers("Fullstack");
+                  setSearchInputValue("Fullstack");
+                }}
+              >
+                {" "}
+                  Fullstack Engineer
+              </span>
+              <span
+                className='px-[12px] py-1 m-2 font-semibold cursor-pointer rounded-[4px] border-[1px] border-[#252234] hover:border-[#716999]'
+                onClick={() => {
+                  fetchDevelopers("Frontend");
+                  setSearchInputValue("Frontend");
+                }}
+              >
+                {" "}
+                  Frontend Engineer
+              </span>
+              <span
+                className='px-[12px] py-1 m-2 font-semibold cursor-pointer rounded-[4px] border-[1px] border-[#252234] hover:border-[#716999]'
+                onClick={() => {
+                  fetchDevelopers("Backend");
+                  setSearchInputValue("Backend");
+                }}
+              >
+                {" "}
+                  Backend Engineer
+              </span>
             </p>
           </div>
-          <div className='flex justify-center py-10 text-center lg:text-left'>
-            <div className='flex flex-col items-center '>
-              <Search
-                className='talentSearch  mr-2 pb-2 md:pb-0'
-                allowClear
-                value={searchInputValue}
-                placeholder='Search by skills & roles'
-                onChange={(value) => {
-                  setSearchInputValue(value.target.value);
-                }}
-                onSearch={(value) => {
-                  fetchDevelopers(value?.toLowerCase());
-                }}
-              />
+        </div>
+       
+      </section>
 
-              <p className='pt-4'>
-                Popular categories:
-                <span
-                  className='px-2 font-semibold cursor-pointer'
-                  onClick={() => {
-                    fetchDevelopers("Fullstack");
-                    setSearchInputValue("Fullstack");
-                  }}
-                >
-                  {" "}
+      <div style={{ position: "relative" }}>
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70%]"
+          style={{
+            width: "1000px",
+            height: "1000px",
+            borderRadius: "50%",
+            background:
+                  "radial-gradient(37.76% 37.76% at 50.00% 38.71%, rgba(231, 180, 255, 0.20) 0%, rgba(137, 118, 254, 0.20) 100%)",
+            filter: "blur(80px)",
+            zIndex: "1",
+            top: "370px",
+            bottom: "auto",
+          }}
+        ></div>
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70%]"
+          style={{
+            width: "1000px",
+            height: "1000px",
+            borderRadius: "50%",
+            background:
+                  "radial-gradient(37.76% 37.76% at 50.00% 38.71%, rgba(231, 180, 255, 0.20) 0%, rgba(137, 118, 254, 0.20) 100%)",
+            filter: "blur(80px)",
+            zIndex: "1",
+            bottom: "-620px",
+            top: "auto",
+          }}
+        ></div>
+        <div className="bg-[#020013] z-40 relative">
+          <div className="relative mt-[100px]" style={{ zIndex: "2" }}>
+            <div className="py-[100px]">
+              {isLoading && (
+                <div className='site-card-wrapper h-screen '>
+                  <Row className='gap-6 mx-10'>
+                    <Skeleton avatar paragraph={{ rows: 4 }} />
+                    <Skeleton avatar paragraph={{ rows: 4 }} />
+                    <Skeleton avatar paragraph={{ rows: 4 }} />
+                  </Row>
+                </div>
+              )}
+              {developerData && developerData.length !== 0 && !isLoading ? (
+                <div className='site-card-wrapper   z-40'>
+                  <Row className='p-2 md:p-0'>
+                    {developerData.map((dev: Developer, index: number) => {
+                      return <TalentPoolList key={index} devCard={dev} />;
+                    })}
+                  </Row>
+                </div>
+              ) : (
+                !isLoading && (
+                  <div className='flex  flex-col justify-center items-center  py-5'>
+                    <svg width="226" height="226" viewBox="0 0 226 226" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g filter="url(#filter0_f_3127_109330)">
+                        <circle cx="113" cy="113" r="37.5" fill="#600000"/>
+                      </g>
+                      <path d="M121.335 124.111L135.223 138M135.223 124.111L121.335 138M110.223 118.555C99.4846 118.555 90.7791 127.261 90.7791 138H110.223M121.335 99.1109C121.335 105.247 116.36 110.222 110.223 110.222C104.087 110.222 99.1124 105.247 99.1124 99.1109C99.1124 92.9744 104.087 87.9998 110.223 87.9998C116.36 87.9998 121.335 92.9744 121.335 99.1109Z" stroke="#FFCCCC" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+                      <defs>
+                        <filter id="filter0_f_3127_109330" x="0.5" y="0.5" width="225" height="225" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                          <feGaussianBlur stdDeviation="37.5" result="effect1_foregroundBlur_3127_109330"/>
+                        </filter>
+                      </defs>
+                    </svg>
+
+                    <p className="text-white  text-[22px] font-[500]  ">No results found</p>
+                    <p className="text-[#908E9F] py-2 text-[18px] text-center">Try shortening or rephrasing your search </p>
+
+                    <p className=' text-[#908E9F] text-[14px] flex flex-col md:flex-row gap-4'>
+                      <span
+                        className='px-[12px] py-1 mx-2 font-semibold cursor-pointer rounded-[4px] border-[1px] border-[#252234] hover:border-[#716999]'
+                        onClick={() => {
+                          fetchDevelopers("Fullstack");
+                          setSearchInputValue("Fullstack");
+                        }}
+                      >
+                        {" "}
                   Fullstack Engineer
-                </span>
-                <span
-                  className='px-2 font-semibold cursor-pointer'
-                  onClick={() => {
-                    fetchDevelopers("Frontend");
-                    setSearchInputValue("Frontend");
-                  }}
-                >
-                  {" "}
+                      </span>
+                      <span
+                        className='px-[12px] py-1 mx-2 font-semibold cursor-pointer rounded-[4px] border-[1px] border-[#252234] hover:border-[#716999]'
+                        onClick={() => {
+                          fetchDevelopers("Frontend");
+                          setSearchInputValue("Frontend");
+                        }}
+                      >
+                        {" "}
                   Frontend Engineer
-                </span>
-                <span
-                  className='px-2 font-semibold cursor-pointer'
-                  onClick={() => {
-                    fetchDevelopers("Backend");
-                    setSearchInputValue("Backend");
-                  }}
-                >
-                  {" "}
+                      </span>
+                      <span
+                        className='px-[12px] py-1 mx-2 font-semibold cursor-pointer rounded-[4px] border-[1px] border-[#252234] hover:border-[#716999]'
+                        onClick={() => {
+                          fetchDevelopers("Backend");
+                          setSearchInputValue("Backend");
+                        }}
+                      >
+                        {" "}
                   Backend Engineer
-                </span>
-              </p>
+                      </span>
+                    </p>
+
+                    <p className="text-[#908E9F] py-2 text-[18px] mt-8 text-center">If the position you searched for is not available in the public pool.
+                      <br/>
+Click the below option to get in touch with us.</p>
+                    <div className='flex gap-4 items-center justify-center mt-10'>
+                      <Link href='/companies/start-hiring?step=1'>
+                        <Button type='primary' className='page-btn' size='large'>
+              Get started for free
+                        </Button>
+                      </Link>
+                    </div>
+
+                  </div>
+                )
+              )}
+              <GetStartedBanner developerData={developerData} isLoading={isLoading} />
             </div>
           </div>
-        </Container>
-      </section>
-      <section className='py-5 bg-light-gray '>
-        <Container>
-          {isLoading && (
-            <div className='site-card-wrapper h-screen '>
-              <Row className='gap-6 mx-10'>
-                <Skeleton avatar paragraph={{ rows: 4 }} />
-                <Skeleton avatar paragraph={{ rows: 4 }} />
-                <Skeleton avatar paragraph={{ rows: 4 }} />
-              </Row>
-            </div>
-          )}
-          {developerData && developerData.length !== 0 && !isLoading ? (
-            <div className='site-card-wrapper   z-40'>
-              <Row className='p-2 md:p-0'>
-                {developerData.map((dev: Developer, index: number) => {
-                  return <TalentPoolList key={index} devCard={dev} />;
-                })}
-              </Row>
-            </div>
-          ) : (
-            !isLoading && (
-              <div className='flex  flex-col justify-center items-center text-xl text-brand-500 py-5'>
-                <p>
-                  The position you searched for is not available in the public
-                  pool.
-                </p>
-                <p>Fill out the form below to get in touch with us.</p>
-              </div>
-            )
-          )}
-        </Container>
-      </section>
+        </div>
+      </div>      
+      <HireService />
 
-      <GetStartedBanner developerData={developerData} isLoading={isLoading} />
-    </>
+    </Container>
   );
 };
 
